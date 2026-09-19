0e15d3a2e0a8bc376d44132f3b74314dc8e52cb5
edson <antoedson@gmail.com>
2026-09-18 21:49:32 -0300

    atualizando Apk

--- backend/src/routes/iaRoutes.js
+++
@@ -1,7 +0,0 @@
-        const express = require('express');
-        const router = express.Router();
-        const iaController = require('../controllers/iaController');
-
-        router.post('/ia/vendas', iaController.perguntarSobreVendas);
-
-        module.exports = router;
